08911ab29f8382c3c097f6db737e14d0cee63223
aikang <1347371844@qq.com>
2024-06-26 20:52:53 +0800

    学习基础数据类型以及常量、变量、静态变量

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let arr = [1, 2, 3];
+    let tup = (1, '2', 'a');
+    // copy
+    let arr_ownership = arr;
+    let tup_ownership = tup;
+    println!("arr {:?}", arr);
+    println!("tup {:?}", tup);
+    println!("arr_ownership {:?}", arr_ownership);
+    println!("tup_ownership {:?}", tup_ownership);
+    /*
+        copy：拷贝
+        move：移动
+            string
+     */
+    let str1 = String::from("abc");
+    let str2 = str1;
+    // 此时会报错，因为str1不存在了
+    println!("str1: {}", str1);
 }
